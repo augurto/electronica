5ab8250995fec8e4bb7edec9381ac2526be6cf8d
Ego <augurto.17@gmail.com>
2021-10-21 00:55:32 -0500

    mejora menu2

--- pages/header.php
+++ pages/header.php
@@ -1,15 +1,27 @@
-<header>
-		<div class="menu_bar">
-			<a href="#" class="bt-menu"><span class="icon-list2"></span>Menu</a>
-		</div>
-
-		<nav>
-			<ul>
-				<li><a href="#"><span class="icon-house"></span>Inicio</a></li>
-				<li><a href="#"><span class="icon-suitcase"></span>Trabajos</a></li>
-				<li><a href="#"><span class="icon-rocket"></span>Proyectos</a></li>
-				<li><a href="#"><span class="icon-earth"></span>Servicios</a></li>
-				<li><a href="#"><span class="icon-mail"></span>Contactos</a></li>
-			</ul>
-		</nav>
-	</header>
+<div class="header">
+    <div class="overlay">
+        <div class="container-custom">
+            <ul class="nav-one">
+                <li><a href="#"><i class="fa fa-heart-o"></i></a></li>
+                <li><a href="#"><i class="fa fa-cart-arrow-down"></i></a></li>
+                <li><a href="#">login / Register</a></li>
+            </ul>
+        </div>
+        <div class="container">
+            <div class="logo-nav">
+                <div class="logo"><a href="#"><span>EM</span>AN LOGO</a></div>
+                <i class="fa fa-bars fa-2x"></i>
+                <ul class="nav-two">
+                    <li class="active">Home</li>
+                    <li>About</li>
+                    <li>Portofolio</li>
+                    <li>Shortcodes</li>
+                    <li>Blog</li>
+                    <li>Contact</li>
+                    <li><i class="fa fa-search"></i></li>
+                    <li class="slide"></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
--- pages/menu/nav.js
+++ pages/menu/nav.js
@@ -1,23 +1,28 @@
-$(document).ready(main);
-
-var contador = 1;
-
-function main(){
-	$('.menu_bar').click(function(){
-		// $('nav').toggle(); 
-
-		if(contador == 1){
-			$('nav').animate({
-				left: '0'
-			});
-			contador = 0;
-		} else {
-			contador = 1;
-			$('nav').animate({
-				left: '-100%'
-			});
-		}
-
-	});
-
-};
+// Navbar ===========================================================================
+function navbar() {
+    var active = $('.nav-two li'),
+        ctrlMenu = $('.logo-nav > .fa');
+    active.on('click', function () {
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+    $('*:not(.logo-nav > .fa, .logo-nav .nav-two, .nav-two li)').click(function () {
+        if (ctrlMenu.hasClass('fa-close')) {
+            ctrlMenu.click();
+        }
+    });
+    ctrlMenu.on('click', function () {
+        $(this).toggleClass('fa-bars fa-close').next('ul').slideToggle();
+    });
+    $('.logo-nav > .fa, .logo-nav .nav-two, .nav-two li').click(function (event) {
+        event.stopPropagation();
+    });
+    $(window).on('resize', function () {
+        if ($(window).innerWidth() >= 1000) {
+            $('.nav-two').css('display', 'block');
+        } else {
+            $('.nav-two').css('display', 'none');
+            ctrlMenu.addClass('fa-bars').removeClass('fa-close');
+        }
+    });
+}
+navbar();

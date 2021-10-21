cd1593963366d264efa5f2fbc84c677161053f3d
Ego <augurto.17@gmail.com>
2021-10-21 02:11:04 -0500

    cambio de ancho la 100%

--- pages/nav.php
+++ pages/nav.php
@@ -33,7 +33,7 @@
     </li>
     <li class="tabItem">
       <a href="" class="tabInner">
-        <div class="thumbnail color6" data-letter="c"></div>
+        <div class="thumbnail color6" data-letter="s"></div>
         <p class="label">Setting</p>
       </a>
     </li>
@@ -44,37 +44,37 @@
 </nav>
 
 <div class="page">
-  <section class="section activeSection color1" data-letter="h">
+  <section class="section activeSection color1" data-letter="i">
     <article class="sectionInner">
       <h1 class="sectionTitle">Home</h1>
       <p>Use Left and Right Arrow Keys for Navigate or Change Tabs. And You Can Click On Tabs to Change!</p>
     </article>
   </section>
-  <section class="section color2" data-letter="a">
+  <section class="section color2" data-letter="v">
     <article class="sectionInner">
       <h1 class="sectionTitle">About Us</h1>
       <p>Use Left and Right Arrow Keys for Navigate or Change Tabs. And You Can Click On Tabs to Change!</p>
     </article>
   </section>
-  <section class="section color3" data-letter="s">
+  <section class="section color3" data-letter="p">
     <article class="sectionInner">
       <h1 class="sectionTitle">Services</h1>
       <p>Use Left and Right Arrow Keys for Navigate or Change Tabs. And You Can Click On Tabs to Change!</p>
     </article>
   </section>
-  <section class="section color4" data-letter="p">
+  <section class="section color4" data-letter="v">
     <article class="sectionInner">
       <h1 class="sectionTitle">Pricing</h1>
      <p>Use Left and Right Arrow Keys for Navigate or Change Tabs. And You Can Click On Tabs to Change!</p>
     </article>
   </section>
-  <section class="section color5" data-letter="c">
+  <section class="section color5" data-letter="r">
     <article class="sectionInner">
       <h1 class="sectionTitle">Contact</h1>
       <p>Use Left and Right Arrow Keys for Navigate or Change Tabs. And You Can Click On Tabs to Change!</p>
     </article>
   </section>
-  <section class="section color6" data-letter="b">
+  <section class="section color6" data-letter="s">
     <article class="sectionInner">
       <h1 class="sectionTitle">Blog</h1>
       <p>Use Left and Right Arrow Keys for Navigate or Change Tabs. And You Can Click On Tabs to Change!</p>

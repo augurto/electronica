d774a105e53630ab677ba221e501e90bfc93f50c
Ego <augurto.17@gmail.com>
2021-10-21 00:40:59 -0500

    posicionando menu para responsive

--- pages/nav.php
+++ pages/nav.php
@@ -6,3 +6,12 @@
             <a href="">Configuracion</a>
         </div>
 </nav>
+<nav>
+			<ul>
+				<li><a href="#"><span class="icon-house"></span>Inicio</a></li>
+				<li><a href="#"><span class="icon-suitcase"></span>Trabajos</a></li>
+				<li><a href="#"><span class="icon-rocket"></span>Proyectos</a></li>
+				<li><a href="#"><span class="icon-earth"></span>Servicios</a></li>
+				<li><a href="#"><span class="icon-mail"></span>Contactos</a></li>
+			</ul>
+</nav>
